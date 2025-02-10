d4b7c86fd9c526307bb534691381cfbd462eab4f
Manin <maro@ozone.email>
2025-02-10 11:55:16 +0900

    echoするように変更

--- js/webview.js
+++ js/webview.js
@@ -5,5 +5,8 @@ function init() {
 }
 
 function nfc_check_click(){
-	Android.showMessage("HELLO WORLD");
+	const msg_text = document.getElementById('msg_text');
+	msg_text.textContent = "読み込み開始";
+	const res = Android.showMessage("HELLO WORLD");
+	msg_text.textContent = res;
 }
